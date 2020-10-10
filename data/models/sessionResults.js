5d9f745997c4cc9504b71c855f92e9f698a1877c
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-10-09 23:16:54 -0700

    adds images to assets folder, begins data modelling

---
+++ data/models/sessionResults.js
@@ -0,0 +1,24 @@
+export const sessionResults = [
+  {
+    user: '',
+    id: data._id,
+    name: data.pokemon,
+    type: data.type_1,
+    timesSeen: 0,
+    timesCaptured: 0
+  }
+]
+
+export const allTimeResults = [
+  {
+    user: sessionResult.user {
+      user: '',
+      id: data._id,
+      name: data.pokemon,
+      pokemonTypes: {
+        type: data.type_1,
+        timesSeen: 0,
+        timesCaptured: 0
+      }
+  }
+]
